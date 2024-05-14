78446765e2adc90f96de7bc8fa4eafbfafc65c7b
AzizuAhmad <azizu.rozaki@gmail.com>
2024-05-14 08:09:07 +0700

    fungsi login dan detailproductViewFav

---
+++ ProjectCat/Views/DetailProductViewFav.swift
@@ -0,0 +1,18 @@
+//
+//  DetailProductViewFav.swift
+//  ProjectCat
+//
+//  Created by MacBook Air on 13/05/24.
+//
+
+import SwiftUI
+
+struct DetailProductViewFav: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailProductViewFav()
+}
