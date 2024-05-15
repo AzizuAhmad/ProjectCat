04ce05385715ffcde6bb018afa5b5cf9a927da60
AzizuAhmad <azizu.rozaki@gmail.com>
2024-05-15 16:36:44 +0700

    favorit logic

---
+++ ProjectCat/Views/InsightDetail.swift
@@ -0,0 +1,18 @@
+//
+//  InsightDetail.swift
+//  ProjectCat
+//
+//  Created by MacBook Air on 14/05/24.
+//
+
+import SwiftUI
+
+struct InsightDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InsightDetail()
+}
